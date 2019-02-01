be2f51d37835c463808e4aa34155b519d6abdf48
RaphSoft <46710552+RaphSoft@users.noreply.github.com>
2019-02-01 21:31:07 +0700

    Add files via upload

---
+++ v1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter your password:" gans;
+
+if [ $gans = gol ] || [ $gans = gol ]
+then
+     cd ygmv1
+     sh select.sh
+           
+    else
+      echo "Sorry, your password is false"
+fi
