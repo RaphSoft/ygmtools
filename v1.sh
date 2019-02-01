c675b4b1fde26be19d539881c3a4748564afa408
RaphSoft <46710552+RaphSoft@users.noreply.github.com>
2019-02-01 21:40:28 +0700

    Update v1.sh

--- v1.sh
+++ v1.sh
@@ -5,6 +5,7 @@ read -p "Enter your password:" gans;
 if [ $gans = gol ] || [ $gans = gol ]
 then
      git clone https://github.com/RaphSoft/toolsv1.git
+     sh select.sh
   
 else
    echo "Sorry your password incorrect"
